891e8bf60a836f665775100df3621872cc49df4a
Егор Гординский <yegorgordinskiy2002@gmail.com>
2022-10-09 23:19:39 +0800

    update

--- src/pages/createTest/CreateTest.jsx
+++ src/pages/createTest/CreateTest.jsx
@@ -54,39 +54,46 @@ function CreateTest({ addTest }) {
           Тест успешно создан!
           <button
             className="create-test__btn-modal"
-            onClick={() => setModal(false)}
+            onClick={(e) => {
+              e.preventDefault();
+              setModal(false);
+            }}
           >
             Ок
           </button>
         </Modal>
         <div className="conteiner">
-          <div className="create-test__inner">
+          <div className="create-test__inner ">
             <h2 className="create-test__title">Создание теста</h2>
             <form className="create-test__form">
-              <input
-                value={test.title}
-                type="text"
-                className="create-test__input"
-                placeholder="Название"
-                onChange={(e) => setTest({ ...test, title: e.target.value })}
-              />
-              <input
-                value={test.description}
-                type="text"
-                className="create-test__input"
-                placeholder="Описание"
-                onChange={(e) =>
-                  setTest({ ...test, description: e.target.value })
-                }
-              />
-              <input
-                value={test.img}
-                type="text"
-                className="create-test__input"
-                placeholder="Ссылка на изображение"
-                onChange={(e) => setTest({ ...test, img: e.target.value })}
-              />
-              <div className="create-test__questions">
+              <div className="create-test__information">
+                <h3>Данные теста</h3>
+                <input
+                  value={test.title}
+                  type="text"
+                  className="create-test__input"
+                  placeholder="Название"
+                  onChange={(e) => setTest({ ...test, title: e.target.value })}
+                />
+                <input
+                  value={test.description}
+                  type="text"
+                  className="create-test__input"
+                  placeholder="Описание"
+                  onChange={(e) =>
+                    setTest({ ...test, description: e.target.value })
+                  }
+                />
+                <input
+                  value={test.img}
+                  type="text"
+                  className="create-test__input"
+                  placeholder="Ссылка на изображение"
+                  onChange={(e) => setTest({ ...test, img: e.target.value })}
+                />
+              </div>
+              <div className="create-test__questions create-test__information">
+                <h3>Вопросы (пссс обязательно сохраните вопрос)</h3>
                 {arrayQuestions}
                 <button className="create-test__btn" onClick={addNewQuestion}>
                   Добавить вопрос
@@ -94,7 +101,8 @@ function CreateTest({ addTest }) {
               </div>
               <button
                 className="create-test__btn"
-                onClick={() => {
+                onClick={(e) => {
+                  e.preventDefault();
                   addTest(test);
                   setModal(true);
                 }}
